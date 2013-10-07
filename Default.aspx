f13126c9aade17f15dbc0ffab43f624f6744243b
Daniel Salamon <daniel@fairweb.dk>
2013-10-07 15:31:38 +0200

    Cart handling -> Saving "Add to Cart" as cookies.

--- Default.aspx
+++ Default.aspx
@@ -1,13 +1,31 @@
-﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+﻿<%@ Page 
+    Title="Home Page" 
+    Language="C#" 
+    MasterPageFile="~/Site.Master" 
+    AutoEventWireup="true" 
+    CodeFile="Default.aspx.cs" 
+    Inherits="_Default" 
+    EnableEventValidation="false"
+%>
 
 <asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
 
     <div>
-        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+        <asp:GridView ID="GridView1" runat="server" onrowcommand="GridView1_RowCommand" AutoGenerateColumns="false">
             <Columns>
-                <asp:BoundField HeaderText="Title" DataField="innerText" />
+                <asp:BoundField DataField="Artist" HeaderText="Artist"/>
+                <asp:BoundField DataField="Title" HeaderText="Title"/>
+                <asp:BoundField DataField="Price" HeaderText="Price" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:Button ID="Button1" runat="server" CommandName="AddToCart" CommandArgument='<%#Bind("ID") %>' Text="Add to cart" />
+                    </ItemTemplate>
+                </asp:TemplateField>
             </Columns>
         </asp:GridView>
+
+        <asp:PlaceHolder runat="server" ID="PlaceHolder1" />
+        
         <br />
         <br />
         Add Album<br />
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
+using System.Data;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,22 +10,63 @@ using System.Web.UI.WebControls;
 public partial class _Default : Page
 {
     XmlDocument vidDocument = new XmlDocument();
-    const string fileName = "|DataDirectory|/data.xml";
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        vidDocument.Load(Server.MapPath("App_Data/data.xml"));
+        if (!Page.IsPostBack)
+        {
+            vidDocument.Load(Server.MapPath("App_Data/data.xml"));
 
-        // this is how you get all the video nodes, perhaps to bind them or iterate over them
-        XmlNodeList videos = vidDocument.SelectNodes("/store/album");
+            XmlNodeList nodeList = vidDocument.GetElementsByTagName("album");
 
-        // this would select all title elements
-        XmlNodeList titles = vidDocument.GetElementsByTagName("title");
-
-        // this binds the gridview to the title text to display
-        GridView1.DataSource = titles;
-        GridView1.DataBind();
-        
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID", typeof(string));
+            dt.Columns.Add("Title", typeof(string));
+            dt.Columns.Add("Artist", typeof(string));
+            dt.Columns.Add("Price", typeof(string));
+            XmlDocument xmldoc = new XmlDocument();
+            foreach (XmlNode node in nodeList)
+            {
+                DataRow dtrow = dt.NewRow();
+                dtrow["ID"] = node["id"].InnerText;
+                dtrow["Title"] = node["title"].InnerText;
+                dtrow["Artist"] = node["artist"].InnerText;
+                dtrow["Price"] = node["price"].InnerText + " kr.";
+                dt.Rows.Add(dtrow);
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
     }
+
+    protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "AddToCart")
+        {
+            int ID = Convert.ToInt32(e.CommandArgument);
+            Boolean newCookie = true;
+            HttpCookie cookie = null;
+
+            if (Request.Cookies["cart_" + ID] != null)
+                newCookie = false;
+
+
+            if (newCookie) // Cookie already exist?
+            {
+                cookie = new HttpCookie("cart_" + ID);
+                cookie["amount"] = "1";
+            }
+            else
+            {
+                cookie = Request.Cookies["cart_" + ID];
+                cookie["amount"] = (int.Parse(cookie["amount"]) + 1).ToString();
+            }
+
+            cookie.Expires = DateTime.Now.AddDays(30d);
+            Response.Cookies.Add(cookie);
+        }
+    }
+
     protected void btnAdd_Click(object sender, EventArgs e)
     {
         XmlNode node = vidDocument.SelectNodes("/store")[0].LastChild;
